8fe468ddf381fbeb12a4a57bc448c49120118fd4
Ben <ben.kneen@gmail.com>
2026-04-30 17:37:24 -0400

    feat: real family and friend customer roster with matched emojis

--- data/customers.js
+++ data/customers.js
@@ -1,14 +1,29 @@
 var CUSTOMERS = [
-  { name: 'Alice',  emoji: '👧' },
-  { name: 'Mom',    emoji: '👩' },
-  { name: 'Dad',    emoji: '👨' },
-  { name: 'Bella',  emoji: '🧒' },
-  { name: 'Mia',    emoji: '👧' },
-  { name: 'Jake',   emoji: '👦' },
-  { name: 'Lily',   emoji: '🧒' },
-  { name: 'Sam',    emoji: '👦' },
-  { name: 'Emma',   emoji: '👧' },
-  { name: 'Leo',    emoji: '👦' }
+  { name: 'Dad',          emoji: '🧔🏻' },   // 40, beard, short brown hair
+  { name: 'Mom',          emoji: '👩🏻' },   // 40, long straight brown hair
+  { name: 'Zoe',          emoji: '🤓' },    // 9, glasses, short curly brown hair
+  { name: 'Mira',         emoji: '👧🏽' },   // 9, curly dark brown hair
+  { name: 'Hazel',        emoji: '👧🏻' },   // 9, Vietnamese, dark brown hair
+  { name: 'Ivy',          emoji: '👧🏼' },   // 9, dirty blonde, freckles
+  { name: 'Liv',          emoji: '👧🏻' },   // 9, straight brown hair, freckles
+  { name: 'Lily',         emoji: '🧒🏻' },   // 9, dirty brown hair
+  { name: 'Sloan',        emoji: '👧' },    // 9, dark curly brown hair
+  { name: 'Papa Greg',    emoji: '👴🏻' },   // 70, bald, glasses
+  { name: 'Nana Karen',   emoji: '👵🏻' },   // 70, glasses, short dark brown hair
+  { name: 'Nana Peggy',   emoji: '👵🏼' },   // 70, glasses, dirty blonde short hair
+  { name: 'Papa Jamie',   emoji: '👴🏻' },   // 70, short gray hair, glasses
+  { name: 'Sue',          emoji: '👵' },    // 70, short dirty blonde
+  { name: 'Aunt Katie',   emoji: '👩🏻' },   // 35, straight brown hair
+  { name: 'Uncle Brennan', emoji: '👨🏻‍🦰' }, // 35, shaved head, red goatee, freckles
+  { name: 'Piper',        emoji: '🧒🏼' },   // 7, curly dirty blonde
+  { name: 'Abigail',      emoji: '🧒🏼' },   // 5, curly strawberry blonde
+  { name: 'Lindsay',      emoji: '👩🏻' },   // 32, straight brown hair
+  { name: 'Kelsey',       emoji: '🧒🏻' },   // 5, straight dark brown hair
+  { name: 'Kayla',        emoji: '👶🏻' },   // 3, very short dark brown hair
+  { name: 'Aunt Lisa',    emoji: '👩🏻‍🦰' },  // 65, long dark red hair
+  { name: 'Aunt Debbie',  emoji: '👩🏻' },   // 65, long light brown hair
+  { name: 'Samira',       emoji: '👩🏾‍🦱' },  // 30, Indian, dark skin, black curly hair
+  { name: 'Sandra',       emoji: '👩🏻' }    // 55, long dark brown hair
 ];
 
 if (typeof module !== 'undefined' && module.exports) {
